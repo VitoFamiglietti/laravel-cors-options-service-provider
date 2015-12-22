e96600af11ed5cf60d5fd48fac02c46cb2f2445e
VitoFamiglietti <vito.famiglietti@gmail.com>
2015-12-22 13:41:17 +0100

    fixed indentation

--- app/Providers/CatchAllOptionsRequestsProvider.php
+++ app/Providers/CatchAllOptionsRequestsProvider.php
@@ -1,19 +1,18 @@
-<?php 
+<?php
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 /**
  * If the incoming request is an OPTIONS request
- * we will register a handler for the requested route
+ * we'll register a handler for the requested route.
+ * Simply we'll return a status 200.
  */
 class CatchAllOptionsRequestsProvider extends ServiceProvider {
-  	
-  	public function register() {
-    	
+
+    public function register() {
     	$request = app('request');
-    	
+
     	if ($request->isMethod('OPTIONS')) {
-      	
       		app()->options($request->path(), function() { return response('', 200); });
     	}
   	}
