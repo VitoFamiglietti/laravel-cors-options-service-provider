fd0f2b59e52561f8bd4dc6a9eb8c3dba7621aa6c
Patrick Arminio <patrick.arminio@gmail.com>
2015-12-22 13:46:14 +0100

    Fix indentation

--- app/Providers/CatchAllOptionsRequestsProvider.php
+++ app/Providers/CatchAllOptionsRequestsProvider.php
@@ -10,10 +10,10 @@ use Illuminate\Support\ServiceProvider;
 class CatchAllOptionsRequestsProvider extends ServiceProvider {
 
     public function register() {
-    	$request = app('request');
+        $request = app('request');
 
-    	if ($request->isMethod('OPTIONS')) {
-      		app()->options($request->path(), function() { return response('', 200); });
-    	}
-  	}
+        if ($request->isMethod('OPTIONS')) {
+            app()->options($request->path(), function() { return response('', 200); });
+        }
+    }
 }
